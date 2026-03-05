7ef3412e53a4c3d161b7180d55084572fe64e6ee
Superstite <snehaagarwal2308@gmail.com>
2026-03-05 16:14:50 +0530

    configs: Adding multiple binaries to a config

--- configs/network/network_config_sneha_executing_all_benchmarks.py
+++ configs/network/network_config_sneha_executing_all_benchmarks.py
@@ -1,31 +1,43 @@
-import m5
 import argparse
 import importlib
-from m5.objects import Root
-from gem5.components.boards.test_board import TestBoard
-#Sneha_Feb11_23
+
+# Sneha_Feb11_23
 import time
-from gem5.components.boards.x86_board import X86Board
+
+import m5
+from m5.objects import Root
+
 from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.boards.test_board import TestBoard
+from gem5.components.boards.x86_board import X86Board
+from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
+    MESITwoLevelCacheHierarchy,
+)
+
+# Sneha_Feb11_23
+# from gem5.components.cachehierarchies.ruby.mi_example_cache_network import MIExampleCacheNetwork
+from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_network import (
+    MESITwoLevelCacheNetwork,
+)
+from gem5.components.memory import (
+    DualChannelDDR4_2400,
+    SingleChannelDDR3_1600,
+)
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.components.processors.linear_generator import LinearGenerator
+from gem5.components.processors.simple_processor import SimpleProcessor
 from gem5.components.processors.simple_switchable_processor import (
     SimpleSwitchableProcessor,
 )
-from gem5.components.processors.cpu_types import CPUTypes
 from gem5.isas import ISA
-from gem5.components.memory import DualChannelDDR4_2400
+
 # from gem5.resources.resource import Resource
-from gem5.resources.resource import Resource, CustomResource
-from gem5.components.processors.simple_processor import SimpleProcessor
-
-from gem5.simulate.simulator import Simulator
+from gem5.resources.resource import (
+    CustomResource,
+    Resource,
+)
 from gem5.simulate.exit_event import ExitEvent
-#Sneha_Feb11_23
-#from gem5.components.cachehierarchies.ruby.mi_example_cache_network import MIExampleCacheNetwork
-from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_network import MESITwoLevelCacheNetwork
-from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import MESITwoLevelCacheHierarchy
-from gem5.components.processors.linear_generator import LinearGenerator
-from gem5.components.memory import SingleChannelDDR3_1600
-
+from gem5.simulate.simulator import Simulator
 
 # size_choices = ["simsmall", "simmedium", "simlarge"]
 
@@ -47,12 +59,14 @@ cache_hierarchy = MESITwoLevelCacheNetwork(
     l1i_assoc=8,
     l2_size="256kB",
     l2_assoc=16,
-    num_l2_banks=2
+    num_l2_banks=2,
 )
 
 memory = DualChannelDDR4_2400(size="8GB")
 
-processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=args.generator_cores)
+processor = SimpleProcessor(
+    cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=args.generator_cores
+)
 
 print("Processor is defined")
 # board = X86Board(
@@ -69,8 +83,53 @@ board = SimpleBoard(
     cache_hierarchy=cache_hierarchy,
 )
 
-binary=CustomResource('/home/sneha/Github_Repos/gem5_Sep_23_25/tests/test-progs/hello/bin/x86/linux/hello')
+binary = CustomResource(
+    "/home/sneha/Github_Repos/gem5_Sep_23_25/tests/test-progs/hello/bin/x86/linux/hello"
+)
 board.set_se_binary_workload(binary=binary)
+
+# binary1 = CustomResource('/home/sneha/Github_Repos/mibench/automotive/susan/susan')
+# arguments1 = ['/home/sneha/Github_Repos/mibench/automotive/susan/susan/input_large.pgm', \
+#               '/home/sneha/Github_Repos/mibench/automotive/susan/output_large.smoothing.pgm', '-s']
+
+# binary2 = CustomResource('/home/sneha/Github_Repos/mibench/automotive/qsort/qsort_large')
+# arguments2 = ['/home/sneha/Github_Repos/mibench/automotive/qsort/input_large.dat']
+
+# binary3 = CustomResource('/home/sneha/Github_Repos/mibench/automotive/bitcount/bitcnts')
+# arguments3 = ['bitcnts', '1125000']
+
+# binary4 = CustomResource('/home/sneha/Github_Repos/mibench/automotive/basicmath/basicmath_small')
+# arguments4 = ['']
+
+# binary5 = CustomResource('/home/sneha/Github_Repos/mibench/telecomm/CRC32/crc')
+# arguments5 = ['/home/sneha/Github_Repos/mibench/telecomm/adpcm/data/large.pcm']
+
+# binary6 = CustomResource('/home/sneha/Github_Repos/mibench/network/dijkstra/dijkstra_large')
+# arguments6 = ['/home/sneha/Github_Repos/mibench/network/dijkstra/input.dat']
+
+# binary7 = CustomResource('/home/sneha/Github_Repos/mibench/network/patricia/patricia')
+# arguments7 = ['/home/sneha/Github_Repos/mibench/network/patricia/large.udp']
+
+# board.set_se_multi_binary_workload(binaries=[binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7,\
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1, binary2, binary3, binary4, binary5, binary6, binary7, \
+#                                             binary1],
+#                                    arguments=[arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1, arguments2, arguments3, arguments4, arguments5, arguments6, arguments7, \
+#                                               arguments1])
 
 # Lastly we run the simulation.
 simulator = Simulator(board=board)
